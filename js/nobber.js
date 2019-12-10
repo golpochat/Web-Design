8fbb66cb8dc689596f7d4c82803835b8f1c56908
Sholorcain <54514850+Sholorcain@users.noreply.github.com>
2019-12-10 20:21:48 +0000

    resizing and repositioning

--- js/nobber.js
+++ js/nobber.js
@@ -1,15 +1,15 @@
 function myFunction() {
   var dots = document.getElementById("dots");
-  var moreReviews = document.getElementById("more");
+  var more = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
     btnText.innerHTML = "More";
-    moreReviews.style.display = "none";
+    more.style.display = "none";
   } else {
     dots.style.display = "none";
     btnText.innerHTML = "Less";
-    moreReviews.style.display = "inline";
+    more.style.display = "inline";
   }
 }
