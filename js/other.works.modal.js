4beb013f28743935d28112743e83b2972e613792
Gearoid de Cleir <gdchot@hotmail.com>
2019-12-15 18:44:01 +0000

    stopped scrolling in mobile verison

--- js/other.works.modal.js
+++ js/other.works.modal.js
@@ -1,64 +1,66 @@
 //modal for first book on mobile:
 
 // Get the modal element
-var modal1 = document.getElementById('simpleModal1');
+var modal1 = document.getElementById("simpleModal1");
 // Get open Modal btn
-var modalBtn1 = document.getElementById('modalBtn1');
+var modalBtn1 = document.getElementById("modalBtn1");
 //get close btn
-var closeBtn1 = document.getElementsByClassName('closeBtn1')[0];
+var closeBtn1 = document.getElementsByClassName("closeBtn1")[0];
 //listen for open click
-modalBtn1.addEventListener('click', openModal1);
+modalBtn1.addEventListener("click", openModal1);
 //listen for close click
-closeBtn1.addEventListener('click', closeModal1);
+closeBtn1.addEventListener("click", closeModal1);
 //listen for outside window click
-window.addEventListener('click', outsideClick1);
-
+window.addEventListener("click", outsideClick1);
 
 //funtion to open modal
-function openModal1(){
-    modal1.style.display = 'block';
+function openModal1() {
+  modal1.style.display = "block";
+  document.body.style.overflow = "hidden";
 }
 //funtion to close modal
-function closeModal1(){
-    modal1.style.display = 'none';
+function closeModal1() {
+  modal1.style.display = "none";
+  document.body.style.overflow = "auto";
 }
 
 //funtion to close modal if outside click
-function outsideClick1(e){
-    if(e.taget == modal1){
-    modal1.style.display = 'none';
-    }
+function outsideClick1(e) {
+  if (e.taget == modal1) {
+    modal1.style.display = "none";
+  }
 }
 
 //modal for second book on mobile:
 // Get the modal element
-var modal2 = document.getElementById('simpleModal2');
+var modal2 = document.getElementById("simpleModal2");
 // Get open Modal btn
-var modalBtn2 = document.getElementById('modalBtn2');
+var modalBtn2 = document.getElementById("modalBtn2");
 //get close btn
-var closeBtn2 = document.getElementsByClassName('closeBtn2')[0];
+var closeBtn2 = document.getElementsByClassName("closeBtn2")[0];
 //listen for open click
-modalBtn2.addEventListener('click', openModal2);
+modalBtn2.addEventListener("click", openModal2);
 //listen for close click
-closeBtn2.addEventListener('click', closeModal2);
+closeBtn2.addEventListener("click", closeModal2);
 //listen for outside window click
-window.addEventListener('click', outsideClick2);
-
+window.addEventListener("click", outsideClick2);
 
 //funtion to open modal
-function openModal2(){
-        modal2.style.display = 'block';
-    }
+function openModal2() {
+  modal2.style.display = "block";
+  document.body.style.overflow = "hidden";
+}
 
 //funtion to close modal
 
-function closeModal2(){
-        modal2.style.display = 'none';
-    }
+function closeModal2() {
+  modal2.style.display = "none";
+  document.body.style.overflow = "auto";
+}
 
 //funtion to close modal if outside click
-function outsideClick2(e){
-        if(e.taget == modal2){
-        modal2.style.display = 'none';
-        }
-    }
+function outsideClick2(e) {
+  if (e.taget == modal2) {
+    modal2.style.display = "none";
+  }
+}
